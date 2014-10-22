5843484a7121a46b412ca3bfe784d8b232e0cab7
kristopher jones <grofernioken@gmail.com>
2014-10-22 08:09:20 -0700

    fixed?

---
+++ public_html/javask/snake.js
@@ -0,0 +1,100 @@
+/*
+ * 
+ * 
+ */
+
+var snake;
+
+var snakeLength;
+
+var snakeSize;
+
+var food;
+
+var context;
+
+var screenWidth;
+
+var screenHeight;
+
+/*
+ * 
+ * 
+ */
+
+gameInitialize();
+snakeInitialize();
+foodInitialize();
+setInterval(gameLoop, 1000/30);
+
+function gameInitialize() {
+    var canvas = document.getElementById("gamescrn");
+    context = canvas.getContext("2d");
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(237, 230, 93)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+/*
+ * 
+ * 
+ */
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push( {
+            x: index,
+            y: 0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    snakeHeadX++;
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
+
+/*
+ * 
+ * 
+ */
+
+function foodInitialize () {
+    food = {
+        x: 0,
+        y: 0
+    };
+}
+
+function foodDraw () {
+    context.fillStyle = "white";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
